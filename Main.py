f0de833037e356c1603f702e1b24e87102718c6b
JONATHAN THOMAS URK21CS2036 <91773936+jonathanthomas19@users.noreply.github.com>
2022-10-12 09:26:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,44 +2,32 @@ from typing import List
 
 def merge_sort(data) -> None:
   # Write code here
-  if len(myList) > 1:
-        mid = len(myList) // 2
-        left = myList[:mid]
-        right = myList[mid:]
+  if len(data)>1:
+        mid = len(data)//2
+        lefthalf = data[:mid]
+        righthalf = data[mid:]
 
-        # Recursive call on each half
-        merge_sort(left)
-        merge_sort(right)
-
-        # Two iterators for traversing the two halves
-        i = 0
-        j = 0
-        
-        # Iterator for the main list
-        k = 0
-        
-        while i < len(left) and j < len(right):
-            if left[i] <= right[j]:
-              # The value from the left half has been used
-              myList[k] = left[i]
-              # Move the iterator forward
-              i += 1
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                data[k]=lefthalf[i]
+                i=i+1
             else:
-                myList[k] = right[j]
-                j += 1
-            # Move to the next slot
-            k += 1
+                data[k]=righthalf[j]
+                j=j+1
+            k=k+1
 
-        # For all the remaining values
-        while i < len(left):
-            myList[k] = left[i]
-            i += 1
-            k += 1
+        while i < len(lefthalf):
+            data[k]=lefthalf[i]
+            i=i+1
+            k=k+1
 
-        while j < len(right):
-            myList[k]=right[j]
-            j += 1
-            k += 1
+        while j < len(righthalf):
+            data[k]=righthalf[j]
+            j=j+1
+            k=k+1
 
 
 # Do not change the following code
